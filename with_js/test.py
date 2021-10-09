0e3e9efbf2a57bc35b0afb16508a2a3d8abd6785
Xuân Vinh Hồ <49198327+hoxuanvinh1999@users.noreply.github.com>
2021-10-09 16:30:47 +0200

    Create test.py

---
+++ with_js/test.py
@@ -0,0 +1,4 @@
+plt.subplot(1,2,2)
+plt.plot(t, power, linewidth=2, linestyle='-', label=r'Power Demand')
+plt.xlabel(r'Time ($s$)', fontsize=22, weight='bold', labelpad=5)
+plt.ylabel(r'Power ($W$)', fontsize=22, weight='bold', labelpad=10)
